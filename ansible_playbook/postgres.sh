568d8408b0a8aa71374d9becdd14be00f45ac283
smartobi <agbawochinonso488@gmail.com>
2023-01-21 19:29:07 +0100

    ansible deploying postgres RDS subnetgroup

--- ansible_playbook/postgres.sh
+++ ansible_playbook/postgres.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aws rds create-db-subnet-group \
-    --db-subnet-group-name mypostgress \
+    --db-subnet-group-name Jumia_Sub_Group_postgress \
     --db-subnet-group-description "Jumia DB subnet group" \
-    --subnet-ids '["subnet-0fc00c2ba14260504","subnet-009b61b3c6ecb5b99","subnet-03eb06d882ab2f736"]'
+    --subnet-ids '["subnet-0a3b2cf38dee9bd78","subnet-0deb0ccf87d458366","subnet-08e4e84f34858d0d0"]'
